5cadee80e078054704a16bb593c4696ed10fb517
Admin <valntn.mrl@gmail.com>
2020-09-22 03:13:24 +0200

    Implement access to Postgresql database.
    - Perform some basic queries to the DB.
    - Disable SSL on connection.
    - Fix a driver problem where '_ "github.com/lib/pq"' is needed as
    import.

--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
+	"log"
 )
 
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	password = "admin"
+	dbname = "test"
+)
+
+
+
+
 func main() {
-	fmt.Printf("%s", "Hello World ! \n")
+	psql := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	fmt.Println(psql)
+
+	db, err := sql.Open("postgres", psql)
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	insertStmt := `insert into "grpc-app"("Name", "age") values('John', 1)`
+	_, e := db.Exec(insertStmt)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	getdata, err := db.Query(`SELECT "age", "Name"  FROM "grpc-app"`)
+	defer getdata.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	for getdata.Next() {
+		var name string
+		var age int
+
+		err = getdata.Scan(&age, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(age, name)
+	}
+
+
 }
